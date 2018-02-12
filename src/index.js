da3e6f5238cdd765bb23150a343492c840e264d3
Joe Haines <hello@joehaines.co.uk>
2018-02-12 14:37:34 +0000

    Add Helmet to deal with stuff in <head>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Helmet from 'react-helmet'
+import React, { Fragment } from 'react'
 import { injectGlobal } from 'styled-components'
 
 import App from './App'
@@ -5,4 +7,15 @@ import globalStyles from './util/global-styles'
 
 injectGlobal`${globalStyles}`
 
-export default App
+export default _ =>
+  <Fragment>
+    <Helmet>
+      <html lang='en' />
+
+      <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
+
+      <title>Joe Haines</title>
+    </Helmet>
+
+    <App />
+  </Fragment>
