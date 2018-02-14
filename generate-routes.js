01d286ab16d96a7ea6dd7624df904247cc4d9a2f
Joe Haines <hello@joehaines.co.uk>
2018-02-14 12:16:32 +0000

    Add first real blog post

--- generate-routes.js
+++ generate-routes.js
@@ -1,8 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 const slug = require('slug')
+const rimraf = require('rimraf')
 const frontMatter = require('front-matter')
 const updateJsonFile = require('update-json-file')
+
+const existingSlugs = require('./package.json').x0.routes
+existingSlugs.forEach(
+  slug => slug !== '/' && rimraf(path.join(__dirname, 'public', slug.replace('/', '')),
+  err => { if (err) throw err })
+)
 
 const slugs = fs.readdirSync(path.join(__dirname, 'src', 'posts')).reverse().map(filename => {
   const contents = fs.readFileSync(path.join(__dirname, 'src', 'posts', filename)).toString()
@@ -12,7 +19,7 @@ const slugs = fs.readdirSync(path.join(__dirname, 'src', 'posts')).reverse().map
 })
 
 updateJsonFile(path.join(__dirname, 'package.json'), data => {
-  data.x0.routes = [...new Set(data.x0.routes.concat(slugs))] // use Set to get a unique list of routes
+  data.x0.routes = slugs
 
   return data
 })
