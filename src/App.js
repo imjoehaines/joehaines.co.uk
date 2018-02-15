3c2d4782ae6bb98564a4411c043d10de6bc26225
Joe Haines <hello@joehaines.co.uk>
2018-02-15 13:17:30 +0000

    Extract blog post list to own component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react'
-import { Link, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import BlogPost from './components/BlogPost'
-import DateComponent from './components/Date'
+import BlogPostList from './components/BlogPostList'
 
 export default ({ basename, pathname, posts }) => (
   <Fragment>
@@ -10,27 +10,7 @@ export default ({ basename, pathname, posts }) => (
       <Route
         exact
         path='/'
-        render={_ =>
-          <Fragment>
-            <header>
-              <h1>Joe Haines <span className='wave'>👋</span></h1>
-            </header>
-
-            <ul className='blog-list'>
-              {posts.map(post =>
-                <li key={post.slug} className='blog-list-item'>
-                  <Link to={post.slug} className='block text-decoration-none'>
-                    <h2 className='blog-list-item__title'>{post.title}</h2>
-
-                    <p className='blog-list-item__description'>{post.description}</p>
-
-                    <small><DateComponent date={post.date} /></small>
-                  </Link>
-                </li>
-              )}
-            </ul>
-          </Fragment>
-        }
+        render={_ => <BlogPostList posts={posts} />}
       />
       {posts.map(post =>
         <Route
@@ -43,7 +23,7 @@ export default ({ basename, pathname, posts }) => (
 
     <footer>
       <small>
-        &copy; {(new Date()).getFullYear()} Joe Haines
+        &copy; {new Date().getFullYear()} Joe Haines
       </small>
     </footer>
   </Fragment>
