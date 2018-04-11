84d7bceb85166a37dbf01a086882a98be5a78fd9
Joe Haines <hello@joehaines.co.uk>
2018-04-11 16:36:49 +0100

    Show anchors for every heading (in markdown)

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,23 +1,20 @@
 import slug from 'slug'
-import React, { createElement } from 'react'
+import React from 'react'
 
 export default ({ level, children, ...props }) => {
-  if (level === 2) {
-    const headingSlug = slug(children, { lower: true })
+  const headingSlug = slug(children, { lower: true })
+  const Heading = `h${level}`
 
-    return (
-      <h2 {...props} id={headingSlug} className={`blog-post__heading ${props.className || ''}`.trim()}>
-        <a
-          href={`#${headingSlug}`}
-          className='blog-post__heading-anchor'
-          title='Jump to this section'
-          aria-hidden
-        />
+  return (
+    <Heading {...props} id={headingSlug} className={`blog-post__heading ${props.className || ''}`.trim()}>
+      <a
+        href={`#${headingSlug}`}
+        className='blog-post__heading-anchor'
+        title='Jump to this section'
+        aria-hidden
+      />
 
-        {children}
-      </h2>
-    )
-  }
-
-  return createElement(`h${level}`, props, children)
+      {children}
+    </Heading>
+  )
 }
