929c76d175a17d63b321ba96f6355e9e58f973c8
Joe Haines <hello@joehaines.co.uk>
2018-04-25 12:54:33 +0100

    Add "home" SVG to blog posts

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -12,7 +12,16 @@ import PostInformation from './PostInformation'
 export default ({ title, date, readingTime, body, previousLink, previousText, nextLink, nextText }) =>
   <article>
     <header>
-      <h1>{title}</h1>
+      <div className='flex'>
+        <h1>{title}</h1>
+
+        <Link href='/' title='Home'>
+          {/* https://iconmonstr.com/home-6-svg/ */}
+          <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'>
+            <path d='M21 13v10h-6v-6h-6v6h-6v-10h-3l12-12 12 12h-3zm-1-5.907v-5.093h-3v2.093l3 3z' />
+          </svg>
+        </Link>
+      </div>
 
       <PostInformation date={date} readingTime={readingTime} />
     </header>
