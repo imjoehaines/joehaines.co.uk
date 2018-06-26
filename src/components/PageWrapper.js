23c9e58ecdbb7faf617d3a2af57e6b805374c079
Joe Haines <hello@joehaines.co.uk>
2018-06-26 15:02:43 +0100

    Add lang attribute to PageWrapper

--- src/components/PageWrapper.js
+++ src/components/PageWrapper.js
@@ -4,7 +4,7 @@ import { Rss } from 'react-feather'
 import Link from './Link'
 
 export default ({ children, description = '', title = '' }) =>
-  <html>
+  <html lang='en'>
     <head>
       <meta charSet='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
