99adc1a173261c28770a0fac690b1906c31e4818
Joe Haines <hello@joehaines.co.uk>
2018-11-05 16:52:40 +0000

    Add dark-mode.js file

---
+++ public/dark-mode.js
@@ -0,0 +1,20 @@
+const toggle = document.querySelector('.toggle-dark-mode')
+
+let isDarkModeEnabled = +window.localStorage.getItem('dark-mode') === 1 || false
+
+toggle.addEventListener('click', function () {
+  isDarkModeEnabled = !isDarkModeEnabled
+
+  render(isDarkModeEnabled)
+
+  window.localStorage.setItem('dark-mode', +isDarkModeEnabled)
+})
+
+function render (isDarkModeEnabled) {
+  document.documentElement.classList.toggle('light-mode', isDarkModeEnabled === false)
+  document.documentElement.classList.toggle('dark-mode', isDarkModeEnabled === true)
+}
+
+render(isDarkModeEnabled)
+
+toggle.classList.remove('hide')
